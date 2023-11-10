44967d761d52b9d0deab995df77ec5107dca5595
sivepanda <sivenpanda@icloud.com>
2023-11-10 03:04:53 -0600

    test

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -3,41 +3,29 @@
 
 	import { onMount, createEventDispatcher } from 'svelte';
 	import { tick } from 'svelte';
-    import {Interactive} from 'svelte-cursor'
+	import { Interactive } from 'svelte-cursor';
 
 	export let gap = 10;
 	export let maxColumnWidth = 250;
 	export let hover = 'false';
-	export let loading;
+	export let loading = 'lazy';
 
 	const dispatch = createEventDispatcher();
 
-	let slotHolder = null;
+	let slotHolder;
 	let columns = [];
-	let galleryWidth = 0;
+	let galleryWidth = 100;
 	let columnCount = 0;
-	var ran = false;
+	var ran = 0;
 
-	$: columnCount = parseInt(galleryWidth / maxColumnWidth) || 1;
-	$: columnCount && Draw();
+	$: columnCount = Math.floor(galleryWidth / maxColumnWidth) || 1;
+	$: columnCount;
 	$: galleryStyle = `grid-template-columns: repeat(${columnCount}, 1fr); --gap: ${gap}px`;
 
-	onMount(Draw);
-
 	async function getNatHeight(img) {
-		return new Promise(function(resolve, reject) {
-			if (img.naturalHeight) {
-				resolve(img.naturalHeight);
-			} else {
-				reject();
-			}
-		});
-	}
-
-	async function consLog(arg) {
-		return new Promise(function(resolve, reject) {
-			if (arg) {
-				resolve(console.log(arg));
+		return new Promise(function (resolve, reject) {
+			if (img.naturalHeight && img.naturalWidth) {
+				resolve((img.naturalHeight) / (img.naturalWidth / galleryWidth));
 			} else {
 				reject();
 			}
@@ -54,59 +42,69 @@
 	}
 
 	async function Draw() {
-		await tick();
+		await tick()
 
 		if (!slotHolder) {
+			console.log('no bueno',columns);
 			return;
 		}
 
-		console.log('drawing 1')
-		console.log('drawing 2')
-		console.log('drawing 3')
+		console.log('drawing 2');
+		console.log('drawing 3');
 
 		const images = Array.from(slotHolder.childNodes).filter((child) => child.tagName === 'IMG');
 		columns = [];
 		var shift = 0;
 
-		console.log('ilen', images.length)
+		console.log('ran', ran);
+		console.log(columns)
 
-		if(!ran){
-			// Fill the columns with image URLs
-			for (let i = 0; (i < images.length); i++) {
+		let promises = images.map((value, i) => {
+			new Promise(async (resolve, reject) => {
 				var idx = (i % columnCount) + shift;
 				const img = new Image();
 				img.src = await images[i].src;
+				columns[idx] = columns[idx]?columns[idx]:[]
 
-				if(columns[idx]){
-					console.log(i, img.src)
-					if(columns[idx].length > 0) {
-						columns[idx]?.push({ src: images[i].src, alt: images[i].alt, class: images[i].className, height: img.naturalHeight })
-						getNatHeight(img).then( async (nh) => {
-							await console.log('nh', nh);
-							await console.log('a', columns[idx].slice(-1)[0].height);
-							await console.log('b', nh + columns[idx + 1].slice(-1)[0].height);
-							
-							if (columns[idx].slice(-1)[0].height + 100 >= nh + columns[idx + 1].slice(-1)[0].height) {
-								await consLog('shift');
-								shift += 1;
-								idx += 1;
-							}
-							await consLog(idx + ' into arr');
-							consLog(columns[idx])
+				console.log(columns, idx, 'ind',i, '\ncolumncount', columnCount, '\ngw', galleryWidth, '\nmcw', maxColumnWidth)
+				
+				const imgObject = {
+						src: images[i].src,
+						alt: images[i].alt,
+						class: images[i].className,
+						height: (img.naturalHeight) / (img.naturalWidth / galleryWidth)
+					}
 
-						});
+				if (columns[idx].length > 0) {
+					const naturalHeight = await getNatHeight(img);
+					if ( idx < columnCount - 1 && columns[idx].slice(-1)[0].height + 100 >= naturalHeight + columns[idx + 1].slice(-1)[0].height ) {
+						await columns[(idx + 1)]?.push(imgObject);
+						console.log((idx+1),'e',imgObject);
+						shift += 1;
+						
+						await resolve(imgObject);
+						
+					} else {
+						await columns[idx]?.push(imgObject);
+						console.log(columns);
+						
+						resolve(imgObject);
 					}
 				} else {
-					console.log('top');
-					columns[idx] = [{ src: images[i].src, alt: images[i].alt, class: images[i].className, height: img.naturalHeight }]
-				}
+					columns[idx] = [imgObject];
+					console.log(columns);
 
-				
-				
-			}
-			ran = true;
-		}
+					await resolve(imgObject);
+				}
+				reject();
+			})
+		});
+
 		
+		return Promise.all(promises).then(values => {
+			console.log('v',values);
+			return columns;
+		});
 	}
 </script>
 
@@ -114,27 +112,30 @@
 	<slot />
 </div>
 
-{#if columns}
-	<div id="gallery" bind:clientWidth={galleryWidth} style={galleryStyle}>
-		{#each columns as column}
-			<div class="column">
-				{#each column as img}
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-                    <Interactive>
-                        <img
-                            src={img.src}
-                            alt={img.alt}
-                            on:click={HandleClick}
-                            class="imag {hover === "true" ? 'img-hover' : ''} {img.class}"
-                            {loading}
-                        />
-                    </Interactive>
-				{/each}
-			</div>
-		{/each}
-	</div>
-{/if}
+<div id="gallery" bind:clientWidth={galleryWidth} style={galleryStyle}>
+
+	{#await Draw()}
+		<div><p>Loading...</p></div>
+	{:then columns}
+			{#each columns as column}
+				<div class="column">
+					{#each column as img}
+						<!-- svelte-ignore a11y-click-events-have-key-events -->
+						<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+						<Interactive>
+							<img
+								src={img.src}
+								alt={img.alt}
+								on:click={HandleClick}
+								class="imag {hover === 'true' ? 'img-hover' : ''} {img.class}"
+								{loading}
+							/>
+						</Interactive>
+					{/each}
+				</div>
+			{/each}
+	{/await}
+</div>
 
 <style>
 	#slotHolder {
@@ -158,13 +159,13 @@
 	}
 	.img-hover {
 		opacity: 0.9;
-        z-index: 1;
+		z-index: 1;
 		transition: all 0.2s;
 	}
 	.img-hover:hover {
-        position:relative;
+		position: relative;
 		opacity: 1;
 		transform: scale(1.5);
-        z-index: 9;
+		z-index: 9;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     import CursorDiv from "../components/CursorDiv.svelte";
     import Perspective from "../components/Perspective.svelte";
     import Gallery from "../components/Gallery.svelte";
-    import IntersectionObserved, { useScrollAction } from "../components/IntersectionObserved.svelte";
+    // import IntersectionObserved, { useScrollAction } from "../components/IntersectionObserved.svelte";
     import { Cursor, Interactive } from 'svelte-cursor';
     import { browser } from '$app/environment';
     import Carousel from 'svelte-carousel';
@@ -28,10 +28,10 @@
         </Carousel>
     {/if}
     <h1 id="her1">Think&<b>Dream</b></h1>
-    <h2>hi. i'm siven panda.</h2>
+    <h2>&gt;&gt; hi. i'm siven panda.</h2>
 </div>
 <div class="fullheight showcasedeck">
-    <h2 class="idea">development</h2>
+    <h2 class="idea">&gt;&gt; development</h2>
     <div>
         <Perspective displayImage="/images/retroim.png" url="https://flyretrograde.com" >
             <img slot="headerim" src="/images/retrograde_wh.png" alt="">
@@ -55,18 +55,17 @@
     
 
 </div>
-<div class="fullheight">
-    <h2 class="idea">photography</h2>
-    <div class="gallery">
-        <Gallery gap={10} hover='true' maxColumnWidth={400}>
-            <img src="/images/photos/DSC_2913.JPG" alt="">
-            <img src="../images/photos/_MG_9697.JPG" alt="">
-            <img src="../images/photos/DSC_6566.JPG" alt="">
-            <img src="../images/photos/DSC_6117.png" alt="">
-            <img src="../images/photos/DSC_5527_Edit.jpg" alt="">
-            <img src="../images/photos/DSC_5347.png" alt="">
-        </Gallery>
+<div class="fullheight gallery">
+    <h2 class="idea">&gt;&gt; photography</h2>
+    <div class="images">
+        <img src="../images/photos/DSC_2913.JPG" alt="">
+        <img src="../images/photos/_MG_9697.JPG" alt="">
+        <img src="../images/photos/DSC_6566.JPG" alt="">
+        <img src="../images/photos/DSC_6117.png" alt="">
+        <img src="../images/photos/DSC_5527_Edit.jpg" alt="">
+        <img src="../images/photos/DSC_5347.png" alt="">
     </div>
+        
 </div>
 
 
@@ -80,6 +79,7 @@
         --purple: #7100b3;
         --pin: #ff00c8;
         --red: #ff0000;
+        --light: #d3d3d3;
         --almostblack: #181818;
     }  
     .herocontainer {
@@ -87,13 +87,13 @@
         left:0px;
         margin-top: -5vh;
         padding-top:0vh;
-        height: 100vh;
+        height: 110vh;
         background:  linear-gradient(rgba(0, 0, 0, 0), var(--almostblack)), url("/images/photos/DSC_3076.JPG");
         background-size:fill;  
         background-position: center;      
     }
     .herocontainer h1 {
-        font-size: 20vh;
+        font-size: 23vh;
         color: white;
         padding: 0px;
         margin: 0px;
@@ -113,7 +113,7 @@
     #her0 {
         background: url("/images/photos/DSC_6492.JPG");
         z-index: 2;
-        margin-top: 10vh;
+        margin-top: 20vh;
         margin-left: 10vw;
         background-position: center;
         -webkit-text-fill-color: transparent;
@@ -139,6 +139,7 @@
         grid-template-rows: 10vh 40vh 40vh;
         align-items: center;
         justify-content: center;
+        padding-bottom: 15vh;
     }
     
     .showcasedeck div {
@@ -158,11 +159,42 @@
     }
 
     .gallery {
-        width: 90vw;
-        padding-left: 5vw;
+        background: var(--light);
+        padding-top: 15vh;
+        padding-bottom: 10vh;
+        width: 99svw;
         display: grid !important;
-        /* flex-direction: column !important; */
         align-items: center !important;
         justify-items: center !important;
+        border-radius: 20px 20px 20px 20px;
+        box-shadow: black 0px -10px 10px;
+    }
+
+    .gallery h2 {
+        color: var(--almostblack);
+        padding-left: 5vw;
+        grid-row: 1;
+        grid-column: 1 !important;
+        align-self: left;
+        justify-self: left;
+    }
+
+    .gallery .images {
+        padding-left: 5vw;
+        padding-right: 5vw;
+    }
+
+    .gallery img {
+        height: 40vh;
+        width: auto;
+        border-radius: 20px;
+        transition: 200ms all ease-in-out;
+    }
+
+    .gallery img:hover {
+        height: 40vh;
+        width: 30vw;
+        background-size: cover;
+        transition: 200ms all ease-in-out;
     }
 </style>
