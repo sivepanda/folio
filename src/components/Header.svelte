824cd49d3c1667485498109955d0e2487e4ecec7
Siven Panda <sivenpanda@icloud.com>
2024-04-12 17:54:49 -0500

    V2 init

---
+++ src/components/Header.svelte
@@ -0,0 +1,56 @@
+<div class="header">
+    <svg version="1.1" id="svg10" viewBox="0 0 614 345" fill="white">
+        <path
+            style="stroke-width:4px;stroke:red;fill:transparent;"
+            d="M 445.48828,82.320312 170.62695,110.33398 v 150.24219 l 155.03711,-15.82422 v -68.55859 l 100.24414,-10.63672 0.30274,-57.60156 -231.42774,22.61719 v 56.1289 l 106.08399,-10.56445 v 57.86523 L 184.04687,246.53711 V 122.58594 L 434.50586,97.070312 v 75.759768 l -99.58203,10.04687 v 40.91016 h 1.59961 v -39.46289 l 99.58203,-10.04688 V 95.298828 L 182.44727,121.14063 V 248.31836 L 302.4668,235.43945 v -61.07031 l -106.08399,10.56445 v -52.91015 l 228.22071,-22.30469 -0.28907,54.39844 -100.25,10.63672 v 68.55273 L 172.22656,258.80469 V 111.7793 L 443.88867,84.091797 V 183.9082 l -98.10547,9.5293 v 19.12109 h 1.59961 v -17.66797 l 98.10547,-9.52929 z"
+            id="path25"
+        />
+    </svg>
+    <div class="right">
+        <a href="/">Home</a>
+        <a href="/">Dev</a>
+        <a href="/">Photography</a>
+    </div>
+</div>
+
+<style>
+    .header {
+        font-family: 'Urbanist', 'sans-serif';
+        font-weight: 650;
+        background: linear-gradient(
+            rgba(31, 31, 46, 1),
+            rgba(31, 31, 46, 0.7) 50%,
+            rgba(0, 0, 0, 0)
+        );
+        backdrop-filter: blur(20px);
+        height: 12vh;
+        width: 100vw;
+        position: fixed;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .right {
+        display: flex;
+        padding-right: 2vw;
+        flex-direction: row;
+    }
+    .right a {
+        display: block;
+        align-self: center;
+        font-size: 3svh;
+        text-decoration: none;
+        color: white;
+        margin: 0;
+        padding: 0;
+        padding-left: 2vw;
+        transition: ease 0.7s;
+    }
+    .right a:hover {
+        margin-top: 0vh;
+        font-weight: 950;
+        /* margin-bottom: 2vh; */
+        color: gray;
+        transition: cubic-bezier(0.57, -0.01, 0, 0.72) 200ms;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+    import Header from '../components/Header.svelte';
+</script>
+
+<Header />
+
+<div class="main">
+    <div class="hero">
+        <h1>Hey, I'm Siven!</h1>
+        <h2>I code, design, and take pictures sometimes</h2>
+        <p>
+            I'm a computer science student at &lbrace; &rbrace;. My goal is to create software and
+            experiences that enable both equity and efficiency across many fields, whether it be
+            health, education, or software development.
+        </p>
+    </div>
+    <div class="dev">
+        <h1>Development</h1>
+        <p>
+            I program in a number of languages and principles, from frontend and design to backend,
+            to simple scripts. Most of my work is done in Java and JS/TS, but I have worked
+            extensively with Python, Rust, C, and C++
+        </p>
+    </div>
+</div>
+
+<style>
+    .main {
+        background: rgb(31, 31, 46);
+        height: 100vh;
+        width: 100vw;
+    }
+
+    .hero {
+        background: linear-gradient(90deg, rgb(31, 31, 46) 40%, rgba(0, 0, 0, 0)),
+            url('/images/DSC_7287.JPG');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: right;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        padding-left: 5vw;
+        padding-bottom: 0px;
+        justify-content: center;
+    }
+
+    .hero > h1 {
+        font-size: 15vh;
+        margin-bottom: 0vh;
+        padding-bottom: 0vh;
+    }
+
+    .hero > h2 {
+        font-family: 'Urbanist', 'sans-serif';
+        font-weight: 450;
+        font-size: 5svh;
+        margin-top: 1vh;
+        margin-bottom: 2vh;
+    }
+
+    .hero > p {
+        width: 40vw;
+        line-height: 1.5;
+    }
+
+    .dev {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        padding-left: 5vw;
+    }
+
+    .dev > h1 {
+        font-size: 15vh;
+    }
+
+    .dev > p {
+        width: 50vw;
+        line-height: 1.5;
+    }
+</style>
