bc4844a9ff7dba508ae2723a4f0bf2991864acb0
Siven Panda <sivenpanda@icloud.com>
2024-10-02 21:28:56 -0400

    GSAP animations for langs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
     import Header from '../components/Header.svelte';
+    import gsap from 'gsap';
+    import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
+
+    onMount(() => {
+		gsap.registerPlugin(ScrollTrigger);
+        console.log("hello")
+		if(browser) {
+			gsap.from('.langbox', {marginLeft: '50%'})
+
+			gsap.timeline({
+				scrollTrigger: {
+					trigger: '.dev',
+					start:'top top',
+					end:'bottom bottom',
+					endTrigger: '.experience',
+					scrub: 1
+				}
+			})
+			.to('.langbox', {marginLeft: '-100%'}, 0)
+		}
+	});
 </script>
 
 <Header />
@@ -9,7 +32,7 @@
         <h1>Hey, I'm Siven!</h1>
         <h2>I code, design, and take pictures sometimes</h2>
         <p>
-            I'm a computer science student at &lbrace; &rbrace;. My goal is to create software and
+            I'm a computer science student at Duke University. My goal is to create software and
             experiences that enable both equity and efficiency across many fields, whether it be
             health, education, or software development.
         </p>
@@ -22,13 +45,29 @@
             extensively with Python, Rust, C, and C++
         </p>
         <h2>Favorite/Frequent Languages and Tools</h2>
-        <p>Java</p>
-        <p>Python</p>
-        <p>TypeScript</p>
-        <p>Svelte</p>
-        <p>Next.js</p>
-        <p>Huggingface</p>
-        <p>Rust</p>
+        <div class="langs">
+            <div class="langbox">
+                <p>Java</p>
+                <p>Python</p>
+                <p>TypeScript</p>
+                <p>Svelte</p>
+                <p>Next.js</p>
+                <p>Huggingface</p>
+                <p>Rust</p>
+            </div>
+        </div>
+    </div>
+    <div class="experience">
+        <h1>Experience</h1>
+        <h2>Oklahoma Medical Research Foundation</h2>
+        <p>
+            Experimented with Transformer and Convolutional Machine Learning models in Python to 
+            determine the viability of a feature reduction model over an entire genomic track.
+        </p>
+
+        Robotics Match Predictor
+
+        Win Predictor for FIRST Robotics with 85% accuracy; Using Sci-kit Learn, I coded cross validation (feature importance) by way of Random Forest Classifiers. In addition, ran a binary classification to train the data on accuracy. Data utilized from Statbotics API.
     </div>
 </div>
 
@@ -72,24 +111,65 @@
         line-height: 1.5;
     }
 
-    .dev {
+    .dev, 
+    .experience {
         height: 100vh;
         display: flex;
         flex-direction: column;
         padding-left: 5vw;
     }
 
-    .dev > h1 {
+    .dev > h1,
+    .experience > h1 {
         font-size: 15vh;
         margin-bottom: .5em;
         padding-bottom: 0;
     }
 
-    .dev > p {
+    .dev > p,
+    .experience > p {
         width: 50vw;
         font-size: 1.2em;
         line-height: 1.5;
         padding-top: 0px;
         margin-top: 0;
     }
+
+    .dev .langs {
+        width: calc(100vw - 5vw - 5vw); 
+        /* padding-bottom: 2vh; */
+        overflow-x: hidden;
+        left: 0;
+        margin-left: 0;
+        height: 24vh;
+        padding-left: -200vw;
+        background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0) 100%);
+        background-clip: text;
+        -webkit-background-clip: text;
+    }
+
+    .langs > .langbox {
+        height: 24vh;
+        padding-bottom: 2vh;
+        margin: 0;
+        /* margin-left: 50%; */
+        padding: 0;
+        display:flex;
+        flex-direction: row;
+        overflow-y: hidden;
+        overflow-x: hidden;
+    }
+
+    .dev .langbox > p {
+        color: transparent;
+        font-size: 5em;
+        font-family: 'Gloock', 'serif' !important;
+        padding-left: 1vw;
+        height: 22vh;
+    }
+
+    .experience {
+        background: linear-gradient(180deg, rgb(31, 31, 46) 0%, rgba(100, 0, 200, 1) 100%);
+    }
+
 </style>
