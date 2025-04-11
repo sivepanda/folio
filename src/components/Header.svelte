61a36983d7da8b9cf034fff69380052d2dcd0104
Siven Panda <sp747@duke.edu>
2025-04-11 17:31:02 -0400

    finally fixed animations, anim on scroll

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <div class="header">
     <svg
     version="1.1"
-    id="svg179"
+    id="svgcontain"
     width="90"
     height="63"
     viewBox="0 0 790.15509 553.80859"
@@ -10,20 +10,20 @@
         id="g185"
         transform="translate(-144.92244,-263.1002)">
         <g
-        id="g1045"
+        id="pthgrp"
         transform="matrix(13.203648,0,0,13.203648,-95.525119,-65.780623)">
         <path
             d="M 18.926311,66.016776 V 41.358024 a 7.7587396,7.7587396 130.64145 0 1 6.582858,-7.669116 l 51.829597,-7.946871 v 11.034219 a 9.7936922,9.7936922 130.56116 0 1 -8.282277,9.676364 l -15.262956,2.384022 v 11.772617 z"
-            class="pth"
+            class="logopath"
             id="path297" 
             />
         <path
             d="m 46.358197,35.881132 -16.590655,2.459531 a 5.047311,5.047311 130.78372 0 0 -4.307147,4.992745 v 7.136755 L 46.58351,47.372106 v 5.4631 A 7.2123416,7.2123416 130.92569 0 1 40.393456,59.97473 L 22.193353,62.580749 V 42.009908 a 5.9040234,5.9040234 130.58981 0 1 4.998716,-5.834202 l 47.273953,-7.335613 v 8.292199 a 6.8074949,6.8074949 130.38167 0 1 -5.714805,6.719227 L 51.25876,46.696167 v 11.153005"
-            class="pth"
+            class="logopath"
             id="path1034" />
         <path
             d="M 51.089775,43.142815 V 35.26152 L 71.53695,31.994479 v 2.703758"
-            class="pth"
+            class="logopath"
             id="path1036" />
         </g>
     </g>
@@ -54,6 +54,7 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+        z-index: 9998;
     }
 
 
@@ -64,16 +65,16 @@
             padding-right: 4vw;
         }
 
-        #svg179 {
+        #svgcontain {
             transfrom: scale(0.2, 0.2);
         }
     }
 
 
-    #svg179 {
+    #svgcontain {
         padding-left: 2vw;
     }
-    #g1045, .pth {
+    #pthgrp, .logopath {
         fill: none;
         stroke: white;
         stroke-width: 1.5;
