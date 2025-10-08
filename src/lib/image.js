73715a66552da230b7e54f8f87a684a92fc77ae0
sivepanda <sivenpanda@icloud.com>
2025-10-08 18:41:32 -0400

    image fix

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,15 +1,14 @@
 import { dev } from '$app/environment';
 
-export function optimize(src, widths = [640, 960, 1280], quality = 90) {
+export function optimize(src, widths = [640, 828, 1200, 1920], quality = 90) {
     if (dev) return src;
 
     return widths
         .slice()
         .sort((a, b) => a - b)
-        .map((width, i) => {
+        .map((width) => {
             const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
-            const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
-            return url + descriptor;
+            return `${url} ${width}w`;
         })
         .join(', ');
 }
