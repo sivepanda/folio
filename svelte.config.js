a67ecb4d1392d8f82c3f234a2f442aad11480707
sivepanda <sivenpanda@icloud.com>
2025-10-08 15:59:06 -0400

    was a little too trigger happy oops

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ export default {
     kit: {
         adapter: adapter({
             images: {
+                sizes: [640, 828, 1200, 1920, 3840],
                 formats: ['image/avif', 'image/webp'],
                 minimumCacheTTL: 300,
                 domains: ['sivenpanda.com', 'folio-mauve.vercel.app'],
